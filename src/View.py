4feef30f0d75160a2aafc5a8c82a68b1f0e7342b
sersajur <sersajur@gmail.com>
2016-06-12 04:11:47 +0300

    add about window

--- src/View.py
+++ src/View.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8 -*-
 from socket import meth
 
-import wx
+import wx, wx.html
 from PlotWidget import PlotWidget
 from datetime import datetime
 
+
 class View(wx.Frame):
+
     def __init__(self):
         self.app = wx.App(False)
 
         wx.Frame.__init__(self, None, title="Nocturnal hypoglycemia prediction", size=(1100, 800))
         panel = wx.Panel(self)
+
+        menu = wx.Menu()
+        about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
+        self.Bind(wx.EVT_MENU, lambda x: self.OnAbout(), about)
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(menu, "&Help")
+        self.SetMenuBar(menuBar)
 
         label = wx.StaticText(panel, label="Train dataset path:", pos=(10, 10))
         self.m_train_dataset_path_edit = wx.TextCtrl(panel, pos=(10, 30), size=(800, 20))
@@ -31,6 +41,12 @@ class View(wx.Frame):
         self.m_log = wx.TextCtrl(panel, pos=(10, 220), size=(360, 480), style=wx.TE_MULTILINE | wx.CB_READONLY)
 
         self.plot = PlotWidget(panel, pos=(400, 220), size=(10, 20))
+
+    def OnAbout(self):
+        dlg = View.AboutBox()
+        dlg.ShowModal()
+        dlg.Destroy()
+
     # Custom bindings
     def SetOnTestAction(self, on_test_action):
         self.Bind(wx.EVT_BUTTON, lambda x: on_test_action(), self.m_test_button)
@@ -62,6 +78,18 @@ class View(wx.Frame):
 
     def ClearGraph(self):
         self.plot.clear()
+
+    class AboutBox(wx.Dialog):
+
+        aboutText = "\nThis application is a part of Master's Thesis \"Prediction of nocturnal hypoglycemia in patients with typr 1 diabetes using mrthods based on decision tree and methods based on neural networks\"\n" \
+                    "\nAuthors: Dmytro Yurchenko and Serhii Sakharov\n" \
+                    "\nKyiv Polytechnic Institute, Department of Applied Math, 2016"
+
+        def __init__(self):
+            wx.Dialog.__init__(self, None, -1, "About Nocturnal hypoglycemia prediction", style=wx.DEFAULT_DIALOG_STYLE|wx.THICK_FRAME|wx.RESIZE_BORDER|wx.TAB_TRAVERSAL)
+            hwin = wx.StaticText(self, label=View.AboutBox.aboutText, size=(400,200))
+            self.CentreOnParent(wx.BOTH)
+            self.SetFocus()
 
     # Run (all settings must be done before this function run)
     def Run(self):
